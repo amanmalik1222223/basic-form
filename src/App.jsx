ac8964ea20e68000b4d8e15b243b6d233ada8c66
amanmalik1709 <aman.malik.ug21@nsut.ac.in>
2024-06-22 09:07:40 +0530

    finishing touches

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,17 @@
-import React, { useState } from 'react';
+// App.js
+import React from 'react';
 import './App.css';
 import Form from './components/Form';
-import Alert from './components/Alert'; // Import Alert component
 
 function App() {
 
-  return (
-    <div className='App'>
-      {/* Render the Form component */}
-      <Form showAlert={showAlert} />
+    
 
-      {/* Render the Alert component conditionally */}
-      {alert.show && <Alert message={alert.message} type={alert.type} />}
-    </div>
-  );
+    return (
+
+                <Form />
+
+    );
 }
 
 export default App;
